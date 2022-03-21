ef90480088c3b3f3ab1fe020b59b5c656bf84644
hamzaaslam2121 <hamzaaslam2121@gmail.com>
2022-03-21 21:42:56 +0000

    py file before beginning database

---
+++ Project1.py
@@ -0,0 +1,8 @@
+"""Creating a Log-in System"""
+
+username = input('Please enter your username')
+password = input('Please enter yout password')
+
+
+def database_check : 
+    if username and password in 
