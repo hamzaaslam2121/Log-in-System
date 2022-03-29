40f4302e0aa37f2ae67b9d1ceac564a2824de709
hamzaaslam2121 <hamzaaslam2121@gmail.com>
2022-03-29 01:45:18 +0100

    Help time

--- Project1.py
+++ Project1.py
@@ -6,7 +6,7 @@ accountsdb = sqlite3.connect(':memory:')
 
 cursor1 = accountsdb.cursor()
 
-cursor1.execute("""CREATE TABLE users (
+cursor1.execute("""CREATE TABLE users(
             username TEXT,
             password TEXT);""")        
 
@@ -17,14 +17,14 @@ pass_word = input('Please enter your password: ')
 
 def database_check(user):   # function for checking whether entered "username" and "password" is already present in database
     cursor1.execute("""SELECT username, password 
-            FROM accountsdb WHERE username=?, password=?""",
+            FROM users WHERE username=?, password=?""",
             (user_name, pass_word))     
 
     result = cursor1.fetchone()
     username_check = cursor1.fetchone(username)
 
     if result is None:
-        cursor1.execute("""INSERT INTO accountsdb (username, password) VALUES (?,?)""", (user_name, pass_word))
+        cursor1.execute("""INSERT INTO users (username, password) VALUES (?,?)""", (user_name, pass_word))
         accountsdb.commit
         print("Welcome to my Log-In System, a new account has been created!")
     elif username_check == 1:
