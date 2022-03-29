dcfff6457046574ac70f5bcd7a1ddb9cad3b523e
hamzaaslam2121 <hamzaaslam2121@gmail.com>
2022-03-29 01:27:56 +0100

    Variable chagnes

--- Project1.py
+++ Project1.py
@@ -7,33 +7,29 @@ accountsdb = sqlite3.connect(':memory:')
 cursor1 = accountsdb.cursor()
 
 cursor1.execute("""CREATE TABLE users (
-            username VARCHAR,
-            password VARCHAR)""")        
+            username TEXT,
+            password TEXT);""")        
 
 
- 
-
-
-
-username = input('Please enter your username ')
-password = input('Please enter your password ')
-
+user_name = input('Please enter your username: ')
+pass_word = input('Please enter your password: ')
 
 
 def database_check(user):   # function for checking whether entered "username" and "password" is already present in database
     cursor1.execute("""SELECT username, password 
             FROM accountsdb WHERE username=?, password=?""",
-            (email, password))     
+            (user_name, pass_word))     
 
     result = cursor1.fetchone()
     username_check = cursor1.fetchone(username)
 
-    if len(result)==0:
-        cursor1.execute("""INSERT INTO accountsdb VALUES (?,?)""", (username, password))
+    if result is None:
+        cursor1.execute("""INSERT INTO accountsdb (username, password) VALUES (?,?)""", (user_name, pass_word))
+        accountsdb.commit
         print("Welcome to my Log-In System, a new account has been created!")
     elif username_check == 1:
         print("Password incorrect, please re-try.")
     else: 
-        return ('Welcome',{}).format(username)
+        return ('Success. Welcome',{}.format(user_name),'!')
 
-    
+accountsdb.close()
