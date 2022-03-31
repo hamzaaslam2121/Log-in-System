de1a21dc742e76b80454102c6125157ecbbf3643
hamzaaslam2121 <hamzaaslam2121@gmail.com>
2022-03-31 15:28:14 +0100

    Program is working with memory conn, but is displaying None as well as return message

--- Project1.py
+++ Project1.py
@@ -17,7 +17,7 @@ def database_check(user_name, pass_word):   # function for checking whether ente
             (user_name, pass_word))
 
     result = cursor1.fetchone()
-    username_check = cursor1.fetchone(user_name)
+    username_check = cursor1.fetchone()
 
     if result is None:
         cursor1.execute("""INSERT INTO users (username, password) VALUES (?,?)""", (user_name, pass_word))
@@ -28,7 +28,7 @@ def database_check(user_name, pass_word):   # function for checking whether ente
     else: 
         return ('Success. Welcome',{}.format(user_name),'!')
 
-user_name = input(str('Please enter your username: ')) # these are the inputs i wish to put into the function (use as args)
+user_name = input(str('Please enter your username: ')) # these are the inputs i put into the function (use as args)
 pass_word = input(str('Please enter your password: '))
 
 print(database_check(user_name, pass_word)) 
