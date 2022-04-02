523217ae4a1af169179f511adbf8256d82d535b3
hamzaaslam2121 <hamzaaslam2121@gmail.com>
2022-04-02 18:09:57 +0100

    Will not process if elif else correctly

--- Project1.py
+++ Project1.py
@@ -2,11 +2,15 @@
 
 import sqlite3
 
-accountsdb = sqlite3.connect(':memory:')
+accountsdb = sqlite3.connect('/Users/hamzaaslam/CompSci/Python/Projects/Project1/Log-in-system-Project/accounts.db')
 
 cursor1 = accountsdb.cursor()
 
-cursor1.execute("""CREATE TABLE users(
+
+user_name = input(str('Please enter your username: ')) # these are the inputs i put into the function (use as args)
+pass_word = input(str('Please enter your password: '))
+
+cursor1.execute("""CREATE TABLE IF NOT EXISTS  users(
             username TEXT,
             password TEXT);""")        
 
@@ -19,18 +23,20 @@ def database_check(user_name, pass_word):   # function for checking whether ente
     result = cursor1.fetchone()
     username_check = cursor1.fetchone()
 
-    if result is None:
+    if username_check == 1 :
+        return("Password incorrect, please re-try.")
+    elif result is None:
         cursor1.execute("""INSERT INTO users (username, password) VALUES (?,?)""", (user_name, pass_word))
         accountsdb.commit
-        print("Welcome to my Log-In System, a new account has been created!")
-    elif username_check == 1:
-        print("Password incorrect, please re-try.")
+        return("Welcome to my Log-In System, a new account has been created!")
     else: 
         return ('Success. Welcome',{}.format(user_name),'!')
 
-user_name = input(str('Please enter your username: ')) # these are the inputs i put into the function (use as args)
-pass_word = input(str('Please enter your password: '))
 
 print(database_check(user_name, pass_word)) 
 
 accountsdb.close()
+
+
+
+
